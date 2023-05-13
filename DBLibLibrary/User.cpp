b9cf87f6c21f7e724ba1d3234fcddee726d1b95b
Kontarn <zek123457@gmail.com>
2023-05-13 18:58:25 +0300

    пустой комит

--- DBLibLibrary/User.cpp
+++ DBLibLibrary/User.cpp
@@ -154,78 +154,192 @@ void User::showAllLinesMass(string**& littleDB, string typeOfLit, bool flag)
  	ifstream fin;
 	User user;
 	string typeLit;
+	size_t pos;
 	string nameBook, nameAutor, yearOfRelease, availability;
+	std::vector <pair<int, string>> littleDBPair, littleDBPair0, littleDBPair1, littleDBPair2;
 	std::vector <std::string> littleDB0, littleDB1, littleDB2;
 	std::string str; // Нужна для временного хранения записи, перед добавленние в вектор
-
+	if (typeOfLit == "Техническая") {
+		typeLit = TechLitDBname;
+	}
+	else typeLit = ArtLitDBname;
 	if (typeOfLit != "Оба типа") {
+		//fin.open(typeLit);
+		//if (fin.is_open()) {
+		//	while (!fin.eof()) {
+		//		getline(fin, str);
+		//		if (str != "")
+		//			littleDB0.push_back(str);
+		//	}
+		//}
+		//fin.close();
 		fin.open(typeLit);
 		if (fin.is_open()) {
 			while (!fin.eof()) {
 				getline(fin, str);
-				if (str != "")
-					littleDB0.push_back(str);
+				if (str != "") {
+					availability = str;
+					pos = availability.find(';');
+					availability.erase(0, pos + 2);
+					littleDBPair.push_back(make_pair(stoi(availability), str));
+				}
 			}
 		}
 		fin.close();
 	}
 	else if (typeOfLit == "Оба типа") {
+		//fin.open(ArtLitDBname);
+		//if (fin.is_open()) {
+		//	while (!fin.eof()) {
+		//		getline(fin, str);
+		//		if (str != "") {
+		//			littleDB1.push_back(str);
+		//		}
+		//	}
+		//}
+		//fin.close();
 		fin.open(ArtLitDBname);
 		if (fin.is_open()) {
 			while (!fin.eof()) {
 				getline(fin, str);
 				if (str != "") {
-					littleDB1.push_back(str);
+					availability = str;
+					pos = availability.find(';');
+					availability.erase(0, pos + 2);
+					littleDBPair1.push_back(make_pair(stoi(availability), str));
 				}
 			}
 		}
 		fin.close();
+		//fin.open(TechLitDBname);
+		//if (fin.is_open()) {
+		//	while (!fin.eof()) {
+		//		getline(fin, str);
+		//		if (str != "") {
+		//			littleDB2.push_back(str);
+		//		}
+		//	}
+		//}
+		//fin.close();
 		fin.open(TechLitDBname);
 		if (fin.is_open()) {
 			while (!fin.eof()) {
 				getline(fin, str);
 				if (str != "") {
-					littleDB2.push_back(str);
+					availability = str;
+					pos = availability.find(';');
+					availability.erase(0, pos + 2);
+					littleDBPair2.push_back(make_pair(stoi(availability), str));
 				}
 			}
 		}
 		fin.close();
-		vector <string> littledb(littleDB1.size() + littleDB2.size());
+		vector <pair<int, string>> littledb(littleDBPair1.size() + littleDBPair2.size());
 
-		std::sort(littleDB1.begin(), littleDB1.end());
-		std::sort(littleDB2.begin(), littleDB2.end());
-		std::set_union(littleDB1.begin(), littleDB1.end(),
-			littleDB2.begin(), littleDB2.end(), littledb.begin());
-		for (std::string i : littledb) {
-			littleDB0.push_back(i);
+	/*	std::sort(littleDB1.begin(), littleDB1.end());
+		std::sort(littleDB2.begin(), littleDB2.end());*/
+		// Отсортруем вектор по первому элементу пары
+		sort(littleDBPair1.begin(), littleDBPair1.end(), [](const pair<int, string>& par, const pair<int, string>& par1) {
+			return par.first < par1.first;
+			});
+		sort(littleDBPair2.begin(), littleDBPair2.end(), [](const pair<int, string>& par, const pair<int, string>& par1) {
+			return par.first < par1.first;
+			});
+		std::set_union(littleDBPair1.begin(), littleDBPair1.end(),
+			littleDBPair2.begin(), littleDBPair2.end(), littledb.begin(), 
+			[](const pair<int, string>& par, const pair<int, string>& par1) {
+				return par.second != par1.second;
+			});
+		for (pair<int, string> i : littledb) {
+			littleDBPair.push_back(make_pair(i.first, i.second));
 		}
 	}
 	// Убираем книги, которых нет в наличии
+	//if (flag == 1) {
+	//	std::vector <std::string> ::iterator it = std::remove_if(littleDB0.begin(),
+	//		littleDB0.end(), [](std::string a) {
+	//			std::size_t pos;
+	//			std::string avail; // наличие в библиотеке
+	//			pos = a.find(";");
+	//			avail = a;
+	//			avail = avail.erase(0, pos + 2);
+	//			return std::stoi(avail) == 0;
+	//		});
+	//	littleDB0.erase(it, littleDB0.end());
+	//}
 	if (flag == 1) {
-		std::vector <std::string> ::iterator it = std::remove_if(littleDB0.begin(),
-			littleDB0.end(), [](std::string a) {
-				std::size_t pos;
-				std::string avail; // наличие в библиотеке
-				pos = a.find(";");
-				avail = a;
-				avail = avail.erase(0, pos + 2);
-				return std::stoi(avail) == 0;
+		// Отсортруем вектор по первому элементу пары
+		sort(littleDBPair.begin(), littleDBPair.end(), [](const pair<int, string>& par, const pair<int, string>& par1) {
+			return par.first < par1.first;
 			});
-		littleDB0.erase(it, littleDB0.end());
+		// result хранит интервал книг, которых нет в наличии
+		pair <vector<pair<int, string>>::iterator, vector<pair<int, string>>::iterator> result;
+		pair<int, string> par{ 0, "" }; // Нужно работы алгоритма equal_range
+		result = equal_range(littleDBPair.begin(), littleDBPair.end(), par,
+			[par](const pair<int, string>& par, const pair<int, string>& par1) {
+				return par.first < par1.first;
+			});
+		// Удаляем все записи, которые имеют наличие 0
+		littleDBPair.erase(result.first, result.second);
 	}
-	this->size = littleDB0.size();
+	this->size = littleDBPair.size();
 	string** LittleDB = DBG_NEW string * [size];
 	for (int i = 0; i < size; i++)
 		LittleDB[i] = DBG_NEW string[4];
 
 	for (int i = 0; i < size; i++) {
-		user.splitEntry(littleDB0[i], nameBook, nameAutor, yearOfRelease, availability);
+		user.splitEntry(littleDBPair[i].second, nameBook, nameAutor, yearOfRelease, availability);
 		LittleDB[i][0] = nameBook;
 		LittleDB[i][1] = nameAutor;
 		LittleDB[i][2] = yearOfRelease;
 		LittleDB[i][3] = availability;
 	}
 	littleDB = LittleDB;
+
+	// -----------------------------------------------------------------------------------------
+	//std::ifstream fin;
+	//std::string str, availability;
+	//
+	////std::vector <pair<int, string>> littledb;
+	//std::string typeLit;
+	//if (typeOfLit == "Техническая") {
+	//	typeLit = TechLitDBname;
+	//}
+	//else typeLit = ArtLitDBname;
+	////std::ifstream fin;
+	////std::string str; // Нужна для временного хранения записи, перед добавленние в вектор
+	//fin.open(typeLit);
+	//if (fin.is_open()) {
+	//	while (!fin.eof()) {
+	//		getline(fin, str);
+	//		if (str != "") {
+	//			availability = str;
+	//			pos = availability.find(';');
+	//			availability.erase(0, pos + 2);
+	//			littledb.push_back(make_pair(stoi(availability), str));
+	//		}
+	//	}
+	//}
+	//fin.close();
+	//// Убираем книги, которых нет в наличии, применяя алгоритм equal_range
+	//if (flag == 1) {
+	//	// Отсортруем вектор по первому элементу пары
+	//	sort(littledb.begin(), littledb.end(), [](const pair<int, string>& par, const pair<int, string>& par1) {
+	//		return par.first < par1.first;
+	//		});
+	//	// result хранит интервал книг, которых нет в наличии
+	//	pair <vector<pair<int, string>>::iterator, vector<pair<int, string>>::iterator> result;
+	//	pair<int, string> par{ 0, "" }; // Нужно работы алгоритма equal_range
+	//	result = equal_range(littledb.begin(), littledb.end(), par,
+	//		[par](const pair<int, string>& par, const pair<int, string>& par1) {
+	//			return par.first < par1.first;
+	//		});
+	//	// Удаляем все записи, которые имеют наличие 0
+	//	littledb.erase(result.first, result.second);
+	//}
+	//for (pair<int, string> i : littledb) {
+	//	littleDB.push_back(i.second);
+	//}
 }
 // Сортировка массива определённым образом
 void User::sortingMass(string**& littleDB, string sortingMethod, size_t size)
